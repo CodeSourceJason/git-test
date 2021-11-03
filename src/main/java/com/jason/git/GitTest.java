58f447a8c54904d7730fdf86fe171a4e9939e524
Jason <zhangjie201319@163.com>
2021-11-03 18:31:04 +0800

    third commit

--- src/main/java/com/jason/git/GitTest.java
+++ src/main/java/com/jason/git/GitTest.java
@@ -12,6 +12,7 @@ public class GitTest {
         System.out.println("Hello Git!");
         System.out.println("Hello Git!2");
         System.out.println("Hello Git!3");
+        System.out.println("Hello Git!4");
 
     }
 }
